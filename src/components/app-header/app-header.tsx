4fdc8f02511258584f208094f66c8cc26ce8daf4
contiki9 <contiki9@gmail.com>
2021-05-14 23:45:40 +0900

    👍  ウインドウサイズでの表示切り替え

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -6,11 +6,25 @@ import { Component, Host, h, State } from "@stencil/core";
   shadow: true
 })
 export class AppHeader {
+  /**
+   * スマホ用のナビゲーションの表示切り替え
+   */
   @State() mobileNav: boolean = false;
+
+  /**
+   * ナビゲーションのモード切替
+   */
+  @State() mode: "short";
 
   private toggleMobileNav = () => {
     this.mobileNav = !this.mobileNav;
   };
+
+  componentDidLoad() {
+    if (document.body.offsetWidth < 690) {
+      this.mode = "short";
+    }
+  }
 
   render() {
     return (
@@ -22,30 +36,59 @@ export class AppHeader {
                 <ion-icon slot="start" name="logo-ionic" />
                 Ionic Japan
               </ion-button>
-              <ion-button href="https://ionicframework.jp/docs">
+              <ion-button
+                class={{
+                  "dom-hide": this.mode === "short" && true
+                }}
+                href="https://ionicframework.jp/docs"
+              >
                 <ion-icon name="library-outline" slot="start" />
                 日本語ドキュメンテーション
               </ion-button>
-              <ion-button href="/case">利用事例</ion-button>
+              <ion-button
+                class={{
+                  "dom-hide": this.mode === "short" && true
+                }}
+                href="/case"
+              >
+                利用事例
+              </ion-button>
             </ion-buttons>
 
             <ion-buttons slot="end">
               <ion-button
+                class={{
+                  "dom-hide": this.mode === "short" && true
+                }}
                 href="https://github.com/ionic-jp/community-site"
                 target="_blank"
               >
                 <ion-icon slot="icon-only" name="logo-github" />
               </ion-button>
               <ion-button
+                class={{
+                  "dom-hide": this.mode === "short" && true
+                }}
                 href="https://twitter.com/ionic_japan"
                 target="_blank"
               >
                 <ion-icon slot="icon-only" name="logo-twitter" />
               </ion-button>
-              <ion-button href="https://ionic-jp.connpass.com/" target="_blank">
+              <ion-button
+                class={{
+                  "dom-hide": this.mode === "short" && true
+                }}
+                href="https://ionic-jp.connpass.com/"
+                target="_blank"
+              >
                 イベント
               </ion-button>
-              <ion-button onclick={this.toggleMobileNav}>
+              <ion-button
+                class={{
+                  "dom-show": this.mode === "short" && true
+                }}
+                onclick={this.toggleMobileNav}
+              >
                 <ion-icon name="ellipsis-vertical-outline" />
               </ion-button>
             </ion-buttons>
--- src/components/app-nav-mobile/test/app-nav-mobile.e2e.ts
+++ src/components/app-nav-mobile/test/app-nav-mobile.e2e.ts
@@ -1,11 +1,11 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage } from "@stencil/core/testing";
 
-describe('app-nav-mobile', () => {
-  it('renders', async () => {
+describe("app-nav-mobile", () => {
+  it("renders", async () => {
     const page = await newE2EPage();
-    await page.setContent('<app-nav-mobile></app-nav-mobile>');
+    await page.setContent("<app-nav-mobile></app-nav-mobile>");
 
-    const element = await page.find('app-nav-mobile');
-    expect(element).toHaveClass('hydrated');
+    const element = await page.find("app-nav-mobile");
+    expect(element).toHaveClass("hydrated");
   });
 });
