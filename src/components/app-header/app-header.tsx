202ff923d55707b9cdec754f449ae93088ddef90
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-05-14 15:06:32 +0900

    chore(): メニューを変更

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -13,20 +13,27 @@ export class AppHeader {
           <ion-toolbar>
             <ion-buttons slot="start">
               <ion-button href="/">
-                <ion-icon slot="icon-only" name="logo-ionic" />
+                <ion-icon slot="start" name="logo-ionic" />
+                Ionic Japan
               </ion-button>
-            </ion-buttons>
-            <ion-title><slot></slot></ion-title>
-            <ion-buttons slot="end">
-              <ion-button fill="outline" href="https://ionicframework.jp/docs">
+              <ion-button href="https://ionicframework.jp/docs">
                 <ion-icon name="library-outline" slot="start"></ion-icon>
                 日本語ドキュメンテーション
               </ion-button>
               <ion-button href="/case">
                 利用事例
               </ion-button>
+            </ion-buttons>
+
+            <ion-buttons slot="end">
               <ion-button href="https://github.com/ionic-jp/community-site" target="_blank">
                 <ion-icon slot="icon-only" name="logo-github" />
+              </ion-button>
+              <ion-button href="https://twitter.com/ionic_japan" target="_blank">
+                <ion-icon slot="icon-only" name="logo-twitter" />
+              </ion-button>
+              <ion-button href="https://ionic-jp.connpass.com/" target="_blank">
+                イベント
               </ion-button>
             </ion-buttons>
           </ion-toolbar>
