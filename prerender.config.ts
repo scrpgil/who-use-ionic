6f872fd1933e22c29efa1b2437058f7f5d1c8382
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-05-14 14:24:18 +0900

    chore

--- prerender.config.ts
+++ prerender.config.ts
@@ -2,7 +2,7 @@ import { PrerenderConfig } from "@stencil/core";
 
 export const config: PrerenderConfig = {
   filterUrl(url) {
-    if (url.pathname.startsWith("/docs") || url.pathname.startsWith("/case")) {
+    if (url.pathname.startsWith("/docs")) {
       return false;
     }
     return true;
